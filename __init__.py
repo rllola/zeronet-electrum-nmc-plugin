b10636062a49710f29bec43c706be7bc2a90ed70
rllola <rllola80@gmail.com>
2021-09-11 14:16:40 +0200

    unzip eletrum zip

--- __init__.py
+++ __init__.py
@@ -1,2 +1,13 @@
+import zipfile
+import os
+
+eletrum_path = os.path.join(os.path.dirname(__file__),'Electrum-NMC-3.3.10')
+
+# Install zip
+if not os.path.exists(eletrum_path):
+  eletrum_zip = os.path.join(os.path.dirname(__file__),'Electrum-NMC-3.3.10.zip')
+  with zipfile.ZipFile(eletrum_zip,'r') as zip_ref:
+    zip_ref.extractall(os.path.join(eletrum_path)
+
 from . import UiRequestPlugin
 from . import SiteManagerPlugin
