1bb6211abf6fccbe175af22a26dfb1dd72810166
rllola <rllola80@gmail.com>
2021-09-11 15:14:03 +0200

    fix path

--- __init__.py
+++ __init__.py
@@ -10,6 +10,7 @@ if not os.path.exists(eletrum_path):
   eletrum_zip = os.path.join(os.path.dirname(__file__),'Electrum-NMC-3.3.10.zip')
   with zipfile.ZipFile(eletrum_zip,'r') as zip_ref:
     zip_ref.extractall(os.path.dirname(__file__))
+  os.rename(os.path.join(os.path.dirname(__file__),'Electrum-NMC-3.3.10'), eletrum_path)
   electrum_executable = os.path.join(eletrum_path, 'run_electrum_nmc')
   st = os.stat(electrum_executable)
   os.chmod(electrum_executable, st.st_mode | stat.S_IEXEC)  
