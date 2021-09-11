64bed3b8f99511c90276a89d3c1c25f5dd1798dd
rllola <rllola80@gmail.com>
2021-09-11 14:33:15 +0200

    fix install

--- __init__.py
+++ __init__.py
@@ -1,13 +1,18 @@
-import zipfile
 import os
 
 eletrum_path = os.path.join(os.path.dirname(__file__),'Electrum-NMC-3.3.10')
 
 # Install zip
 if not os.path.exists(eletrum_path):
+  import stat
+  import zipfile
+
   eletrum_zip = os.path.join(os.path.dirname(__file__),'Electrum-NMC-3.3.10.zip')
   with zipfile.ZipFile(eletrum_zip,'r') as zip_ref:
-    zip_ref.extractall(os.path.join(eletrum_path)
+    zip_ref.extractall(os.path.dirname(__file__))
+  electrum_executable = os.path.join(eletrum_path, 'run_electrum_nmc')
+  st = os.stat(electrum_executable)
+  os.chmod(electrum_executable, st.st_mode | stat.S_IEXEC)  
 
 from . import UiRequestPlugin
 from . import SiteManagerPlugin
